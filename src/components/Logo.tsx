0828022dfca0eb7404c42b68f3324ddbae50fb6d
Julio Zittei <julio.zittei96@gmail.com>
2023-07-17 14:30:54 -0300

    chore: fix project imports and add commit message linter

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import Scissors from "@/assets/scissors.svg";
 export function Logo() {
   return (
     <div className="flex flex-row items-center justify-center gap-1">
-      <Image src={Scissors} alt="tesoura de barbearia" />{" "}
+      <Image src={Scissors} alt="tesoura de barbearia" />
       <span className="font-alt text-xl">Barber Shop</span>
     </div>
   );
