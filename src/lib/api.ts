86e8aaeb451212b2d37811a9e0c1ba62dec27971
Julio Zittei <julio.zittei96@gmail.com>
2023-07-25 02:56:41 -0300

    feat: modify api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `${process.env.BASE_URL ?? "http://localhost:3000/api"}`,
+  baseURL: `${
+    process.env.BASE_URL ?? "https://projeto-barber-frontend-dev.vercel.app/api"
+  }`,
 });
